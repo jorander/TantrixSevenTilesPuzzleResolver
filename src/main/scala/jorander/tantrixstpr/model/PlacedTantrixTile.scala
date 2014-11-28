42a7b3538bd9ef4c53d8ab98d72b46f846bb2210
Jörgen Andersson <jorgen.x.andersson@gmail.com>
2014-11-28 18:27:26 +0100

    Further refactoring of validation code.

--- src/main/scala/jorander/tantrixstpr/model/PlacedTantrixTile.scala
+++ src/main/scala/jorander/tantrixstpr/model/PlacedTantrixTile.scala
@@ -3,21 +3,18 @@ package jorander.tantrixstpr.model
 import jorander.scalarop._
 
 case class PlacedTantrixTile(tile: TantrixTile, nbrOfRotationSteps: Int) {
-  validateConstructorParams(ConstructorParams(tile, nbrOfRotationSteps)) match {
-    case Failure(s) => throw new IllegalArgumentException(s)
-    case Success(_) => /*Do nothing */
-  }
+  (tile, nbrOfRotationSteps) ->> validate(
+    mustNotBeNull[(TantrixTile, Int)](_._1, "Tile"),
+    (params: (TantrixTile, Int)) => if (params._2 >= 0 && params._2 <= 5)
+      succeed(params) else fail("Number of Rotation Steps must be between 0 and 5 (inclusive)")) match {
+      case Failure(s) => throw new IllegalArgumentException(s.mkString("|"))
+      case Success(_) => /*Do nothing since assignment is implicit */
+    }
 
-  private case class ConstructorParams(tile: TantrixTile, nbrOfRotationSteps: Int)
-  private def validateConstructorParams(params: ConstructorParams) =
-    params ->> validatorsWithConcatenatedErrorstrings(
-      (params) => if (params.tile != null) succeed(params) else fail("Tile cannot be null."),
-      (params) => if (params.nbrOfRotationSteps >= 0 && params.nbrOfRotationSteps <= 5)
-        succeed(params) else fail("Number of Rotation Steps must be between 0 and 5 (inclusive)"))
-
-  private def concatErrors(fs: String*) = fs.toList.mkString("|")
-  private def validatorsWithConcatenatedErrorstrings = plus(
-    (s1: ConstructorParams, s2: ConstructorParams) => s2, concatErrors)_
+  private def concatErrors(fs: String*) = fs.toList
+  private def validate[I] = plus((i1: I, i2: I) => i2, concatErrors)_
+  private def mustNotBeNull[I](dataAccessor: I => Any, attributeName: String)(input: I) =
+    if (dataAccessor(input) != null) succeed(input) else fail(attributeName + " cannot be null.")
 
   def bandColor(edgeAsPlaced: TileEdge) = {
     val edgePositions = List(TOP, TOP_RIGHT, BOTTOM_RIGHT, BOTTOM, BOTTOM_LEFT, TOP_LEFT)
