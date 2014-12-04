88fffad3f91b94488e399f4171db138f4f297f29
Jörgen Andersson <jorgen.x.andersson@gmail.com>
2014-12-04 06:08:27 +0100

    Added model class for describing an ongoing seven tiles puzzel and a function for determining wether a new tile can be correctly added to the already placed tiles.

---
+++ src/main/scala/jorander/tantrixstpr/model/SevenTilesPuzzel.scala
@@ -0,0 +1,34 @@
+package jorander.tantrixstpr.model
+
+sealed abstract trait TilePosition
+case object CENTER_POSITION extends TilePosition
+case object LEFT_POSITION extends TilePosition
+case object TOP_RIGHT_POSITION extends TilePosition
+case object TOP_LEFT_POSITION extends TilePosition
+case object RIGHT_POSITION extends TilePosition
+case object BOTTOM_RIGHT_POSITION extends TilePosition
+case object BOTTOM_LEFT_POSITION extends TilePosition
+
+case class SevenTilesPuzzel(placedTiles: Map[TilePosition, PlacedTantrixTile], unplaceTiles: List[TantrixTile])
+
+object SevenTilesPuzzel {
+  private val tileIntersectionsToCheck: Map[TilePosition, List[(TileEdge, (TilePosition, TileEdge))]] = Map(
+    CENTER_POSITION -> List(), // No checks for CenterPosition since it is the first tile
+    TOP_RIGHT_POSITION -> List((BOTTOM_LEFT_EDGE, (CENTER_POSITION, TOP_RIGHT_EDGE))),
+    RIGHT_POSITION -> List((LEFT_EDGE, (CENTER_POSITION, RIGHT_EDGE)), (TOP_LEFT_EDGE, (TOP_RIGHT_POSITION, BOTTOM_RIGHT_EDGE))),
+    BOTTOM_RIGHT_POSITION -> List((TOP_LEFT_EDGE, (CENTER_POSITION, BOTTOM_RIGHT_EDGE)), (TOP_RIGHT_EDGE, (RIGHT_POSITION, BOTTOM_LEFT_EDGE))),
+    BOTTOM_LEFT_POSITION -> List((TOP_RIGHT_EDGE, (CENTER_POSITION, BOTTOM_LEFT_EDGE)), (RIGHT_EDGE, (BOTTOM_RIGHT_POSITION, LEFT_EDGE))),
+    LEFT_POSITION -> List((RIGHT_EDGE, (CENTER_POSITION, LEFT_EDGE)), (BOTTOM_RIGHT_EDGE, (BOTTOM_LEFT_POSITION, TOP_LEFT_EDGE))),
+    TOP_LEFT_POSITION -> List((BOTTOM_RIGHT_EDGE, (CENTER_POSITION, TOP_LEFT_EDGE)), (BOTTOM_LEFT_EDGE, (LEFT_POSITION, TOP_RIGHT_EDGE)), (RIGHT_EDGE, (TOP_RIGHT_POSITION, LEFT_EDGE))))
+
+  def isNewTilePlacementValid(placedTiles: Map[TilePosition, PlacedTantrixTile], position: TilePosition, tile: PlacedTantrixTile): Boolean =
+    (tileIntersectionsToCheck.getOrElse(position, throw new IllegalArgumentException("Unknown position: " + position)) match {
+      case Nil => List(true)
+      case checksToMake => checksToMake.map(_ match {
+        case (tileEdgeToCheck, (otherTilePosition, otherTileEdge)) =>
+          placedTiles.getOrElse(otherTilePosition,
+            throw new IllegalStateException("Tile in position " + otherTilePosition + " must be placed first."))
+            .bandColor(otherTileEdge) == tile.bandColor(tileEdgeToCheck)
+      })
+    }).reduce(_ && _)
+}
---
+++ src/test/scala/jorander/tantrixstpr/model/SevenTilesPuzzelSpec.scala
@@ -0,0 +1,70 @@
+package jorander.tantrixstpr.model
+
+import org.scalatest.Matchers
+import org.scalatest.FlatSpec
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+import jorander.tantrixstpr.model.SevenTilesPuzzel._
+import jorander.tantrixstpr.model.TantrixTile._
+
+@RunWith(classOf[JUnitRunner])
+class SevenTilesPuzzelSpec extends FlatSpec with Matchers {
+  "Function isNewTilePlacementValid" should "always accept the first Tile in the Center position" in {
+    isNewTilePlacementValid(Map(), CENTER_POSITION, PlacedTantrixTile(tile(1), 0)) should be(true)
+  }
+
+  it should "accept the Tile in the TopRight position if it matches the Tile in the Center position" in {
+    isNewTilePlacementValid(Map(CENTER_POSITION -> PlacedTantrixTile(tile(1), 0)), TOP_RIGHT_POSITION, PlacedTantrixTile(tile(8), 0)) should be(true)
+    isNewTilePlacementValid(Map(CENTER_POSITION -> PlacedTantrixTile(tile(1), 1)), TOP_RIGHT_POSITION, PlacedTantrixTile(tile(8), 0)) should be(false)
+  }
+
+  it should "not evaluate a Tile in the TopRight position if no Tile has been placed in Center position" in {
+    a[IllegalStateException] should be thrownBy {
+      isNewTilePlacementValid(Map(), TOP_RIGHT_POSITION, PlacedTantrixTile(tile(1), 0))
+    }
+  }
+
+  it should "accept the Tile in the Right position if it matches the Tile in the Center position and the Tile in the TopRight position" in {
+    isNewTilePlacementValid(Map(CENTER_POSITION -> PlacedTantrixTile(tile(17), 1), TOP_RIGHT_POSITION -> PlacedTantrixTile(tile(1), 1)), RIGHT_POSITION, PlacedTantrixTile(tile(8), 5)) should be(true)
+    isNewTilePlacementValid(Map(CENTER_POSITION -> PlacedTantrixTile(tile(17), 1), TOP_RIGHT_POSITION -> PlacedTantrixTile(tile(1), 1)), RIGHT_POSITION, PlacedTantrixTile(tile(8), 3)) should be(false)
+    isNewTilePlacementValid(Map(CENTER_POSITION -> PlacedTantrixTile(tile(17), 1), TOP_RIGHT_POSITION -> PlacedTantrixTile(tile(1), 1)), RIGHT_POSITION, PlacedTantrixTile(tile(8), 1)) should be(false)
+  }
+
+  it should "not evaluate a Tile in the Right position if no Tile has been placed in Center position" in {
+    a[IllegalStateException] should be thrownBy {
+      isNewTilePlacementValid(Map(TOP_RIGHT_POSITION -> PlacedTantrixTile(tile(1), 1)), RIGHT_POSITION, PlacedTantrixTile(tile(8), 5))
+    }
+  }
+
+  it should "not evaluate a Tile in the Right position if no Tile has been placed in TopRight position" in {
+    a[IllegalStateException] should be thrownBy {
+      isNewTilePlacementValid(Map(CENTER_POSITION -> PlacedTantrixTile(tile(17), 1)), RIGHT_POSITION, PlacedTantrixTile(tile(8), 5))
+    }
+  }
+
+  it should "accept the Tile in the BottomRight position if it matches the Tile in the Center position and the Tile in the Right position" in {
+    isNewTilePlacementValid(Map(CENTER_POSITION -> PlacedTantrixTile(tile(17), 2), RIGHT_POSITION -> PlacedTantrixTile(tile(1), 2)), BOTTOM_RIGHT_POSITION, PlacedTantrixTile(tile(8), 0)) should be(true)
+    isNewTilePlacementValid(Map(CENTER_POSITION -> PlacedTantrixTile(tile(17), 2), RIGHT_POSITION -> PlacedTantrixTile(tile(1), 2)), BOTTOM_RIGHT_POSITION, PlacedTantrixTile(tile(8), 4)) should be(false)
+    isNewTilePlacementValid(Map(CENTER_POSITION -> PlacedTantrixTile(tile(17), 2), RIGHT_POSITION -> PlacedTantrixTile(tile(1), 2)), BOTTOM_RIGHT_POSITION, PlacedTantrixTile(tile(8), 2)) should be(false)
+  }
+  
+  it should "accept the Tile in the BottomLeft position if it matches the Tile in the Center position and the Tile in the BottomRight position" in {
+    isNewTilePlacementValid(Map(CENTER_POSITION -> PlacedTantrixTile(tile(17), 3), BOTTOM_RIGHT_POSITION -> PlacedTantrixTile(tile(1), 3)), BOTTOM_LEFT_POSITION, PlacedTantrixTile(tile(8), 1)) should be(true)
+    isNewTilePlacementValid(Map(CENTER_POSITION -> PlacedTantrixTile(tile(17), 3), BOTTOM_RIGHT_POSITION -> PlacedTantrixTile(tile(1), 3)), BOTTOM_LEFT_POSITION, PlacedTantrixTile(tile(8), 5)) should be(false)
+    isNewTilePlacementValid(Map(CENTER_POSITION -> PlacedTantrixTile(tile(17), 3), BOTTOM_RIGHT_POSITION -> PlacedTantrixTile(tile(1), 3)), BOTTOM_LEFT_POSITION, PlacedTantrixTile(tile(8), 3)) should be(false)
+  }
+  
+  it should "accept the Tile in the Left position if it matches the Tile in the Center position and the Tile in the BottomLeft position" in {
+    isNewTilePlacementValid(Map(CENTER_POSITION -> PlacedTantrixTile(tile(17), 4), BOTTOM_LEFT_POSITION -> PlacedTantrixTile(tile(1), 4)), LEFT_POSITION, PlacedTantrixTile(tile(8), 2)) should be(true)
+    isNewTilePlacementValid(Map(CENTER_POSITION -> PlacedTantrixTile(tile(17), 4), BOTTOM_LEFT_POSITION -> PlacedTantrixTile(tile(1), 4)), LEFT_POSITION, PlacedTantrixTile(tile(8), 0)) should be(false)
+    isNewTilePlacementValid(Map(CENTER_POSITION -> PlacedTantrixTile(tile(17), 4), BOTTOM_LEFT_POSITION -> PlacedTantrixTile(tile(1), 4)), LEFT_POSITION, PlacedTantrixTile(tile(8), 4)) should be(false)
+  }
+  
+  it should "accept the Tile in the TopLeft position if it matches the Tile in the Center position, the Tile in the Left position and the Tile in the TopRight position" in {
+    isNewTilePlacementValid(Map(CENTER_POSITION -> PlacedTantrixTile(tile(17), 5), LEFT_POSITION -> PlacedTantrixTile(tile(1), 5), TOP_RIGHT_POSITION -> PlacedTantrixTile(tile(22),2)), TOP_LEFT_POSITION, PlacedTantrixTile(tile(8), 3)) should be(true)
+    isNewTilePlacementValid(Map(CENTER_POSITION -> PlacedTantrixTile(tile(17), 5), LEFT_POSITION -> PlacedTantrixTile(tile(1), 5), TOP_RIGHT_POSITION -> PlacedTantrixTile(tile(22),2)), TOP_LEFT_POSITION, PlacedTantrixTile(tile(8), 1)) should be(false)
+    isNewTilePlacementValid(Map(CENTER_POSITION -> PlacedTantrixTile(tile(17), 5), LEFT_POSITION -> PlacedTantrixTile(tile(1), 5), TOP_RIGHT_POSITION -> PlacedTantrixTile(tile(56),5)), TOP_LEFT_POSITION, PlacedTantrixTile(tile(8), 5)) should be(false)
+    isNewTilePlacementValid(Map(CENTER_POSITION -> PlacedTantrixTile(tile(17), 5), LEFT_POSITION -> PlacedTantrixTile(tile(1), 5), TOP_RIGHT_POSITION -> PlacedTantrixTile(tile(22),5)), TOP_LEFT_POSITION, PlacedTantrixTile(tile(8), 3)) should be(false)
+  }
+}
